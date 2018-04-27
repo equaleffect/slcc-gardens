60c90edc4163331296047147c49991353a4ded41
rickysok <rickysok@yahoo.com>
2018-04-27 13:06:03 -0600

    Adding Bootstrap to style and format to the bulletin post. Only tested on large monitor and laptop screen. Not ready for mobile.

--- classifieds/classifieds_landing.php
+++ classifieds/classifieds_landing.php
@@ -9,6 +9,7 @@ session_start();
         <meta charset="UTF-8">
         <link href="../css/style.css" rel="stylesheet" type="text/css"/>
         <link href="../css/header.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 
         <script>
             //Script is used to call the search_bulletin.php file to retrieve bulletins based on search input
@@ -40,20 +41,21 @@ session_start();
     ?>
 
     <body class="classifiedsContainer" style="overflow: auto !important"> 
-        <div class="overlayContainerFull">
-            <br>
-            <h2 style="color: white">SLCC Gardens Classifieds</h2>
-            <h3 style="color: white">Search Classifieds</h3>
 
-            <!--Seach form to filter bulletins-->
-            <form name="SearchForm" onsubmit="return SearchBulletins()" style="color: white">
-                <label>Title: </label>
-                <input type="text" name="SearchTitle">
-                <label style="margin-left: 25px">Description: </label>
-                <input type="text" name="SearchDescription">
-                <input type="submit" value="Search" name="Search" style="margin-left: 25px">
-            </form>
-            <br>
+        <div class="col-sm-8 col-sm-offset-2" style="background-color: rgba(0,0,0,.6); padding: 15px">
+            <div class="col-sm-12">
+                <p class="col-sm-8" style="color: white; font-size: 28px;">SLCC Gardens Classifieds</p>
+
+                <!-- Seach form to filter bulletins-->
+                <form class="col-sm-12" name="SearchForm" onsubmit="return SearchBulletins()" style="margin-bottom: 15px">
+                    <label style="color: white">Search Classifieds: </label>
+                    <label style="color: white; margin-left: 25px">Title: </label>
+                    <input type="text" name="SearchTitle">
+                    <label style="color: white;margin-left: 25px">Description: </label>
+                    <input type="text" name="SearchDescription">
+                    <input type="submit" value="Search" name="Search" style="margin-left: 25px">
+                </form>
+            </div>
 
             <div id="BulletinSearch">
                 <?php
@@ -66,7 +68,7 @@ session_start();
 
                 if ($bulletinSearchQueryResult->num_rows > 0) {
                     //Table to store each bulletin
-                    echo "<table>";
+
                     while ($bulletin = $bulletinSearchQueryResult->fetch_assoc()) {
                         $title = $bulletin['Title'];
                         $description = $bulletin['Description'];
@@ -74,21 +76,19 @@ session_start();
 
                         //Style and format each individual pod
                         echo <<< HTML
-                    <tr>
-                        <td>
-                            <div style="color: white">
-                            <div>
-                            <img src="../img/no-img-available.jpg" alt=""" style="width: 300px; height: 300px">
-                            <br>
-                            <h5>Title: $title</h5>
-                            <h5>Description: $description</h5>
-                            <img src="../img/profile-holder.jpg" alt=""" style="width: 30px; height: 30px">
-                            <h5>User Name (Placeholder)</h5>  
-                            <h5>Created: $createdDate</h5>    
-                            <br/>
+                    
+                            <div class="col-sm-4">
+                                <div class="panel panel-default" style="min-height: 450px">
+                                    <div class="panel-heading panelHeading">$title</div>
+                                    <div class="panel-body">
+                                        <img class="img-responsive" src="../img/no-img-available.jpg" alt=""" style="width: 350px; height: 250px">
+                                        <br>
+                                        <p><b>Description: </b> $description</p>
+                                        <h5><b>User: </b>User Name (Placeholder)</h5>  
+                                        <h5><b>Created: </b>$createdDate</h5>   
+                                    </div>
+                                </div>                        
                             </div>
-                        </td>    
-                    </tr>        
 HTML;
                     }
                     //End Table
@@ -100,5 +100,6 @@ HTML;
         <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="../js/header.js"></script>
         <script src="../js/nav.js"></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
     </body>
 </html>
